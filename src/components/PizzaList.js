9006b3bffba8c794056f1eaa82ec4a2d4d587da5
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-01-09 15:02:24 -0500

    completed

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ pizzas, handleClick }) {
+
+  // const [pizzas, setPizzas] = useState([]); //important that this is empty
+
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/pizzas") //first promise
+  //   .then((r) => r.json()) //second promise
+  //   .then((pizzaData) => setPizzas(pizzaData)); //iteration
+  // },[])
+
   return (
     <table className="table table-striped">
       <thead>
@@ -14,7 +24,9 @@ function PizzaList() {
       </thead>
       <tbody>
         {
-          //render Pizza here
+          pizzas.map((pizza) => {
+            return <Pizza key={pizza.id} {...pizza} handleClick={handleClick} />
+          })
         }
       </tbody>
     </table>
